cdcb42cf99b4a2c72da42cf6429e05df39b97cab
lycbrian <brian.oozou.com>
2022-07-19 17:44:26 +0700

    chore: update example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -15,11 +15,11 @@ module "ec2_instance" {
 
   ami                         = "ami-02d1e544b84bf7502"
   instance_type               = "t2.nano"
-  vpc_id                      = "vpc-07ba2496c03937e14"
-  subnet_id                   = "subnet-068288d493755aff2"
+  vpc_id                      = "vpc-xxxxxxxx"
+  subnet_id                   = "subnet-xxxxxxxx"
   is_batch_run                = false # Default is `false`, If machine is need to be `terminated` with instance_initiated_shutdown_behavior
   #key_name                    = "ssh-key"
-  additional_sg_attacment_ids = ["sg-0f587ee2534a49013"] # The sg to associate with this instance
+  additional_sg_attacment_ids = ["sg-xxxxxxx"] # The sg to associate with this instance
   is_create_default_profile = true
 
   user_data = "pwd"
